4e6717bb41862d003064e952184d787bbe1ce750
ladan taheri <taheriprogrammer110@gmail.com>
2026-06-24 15:59:50 +0330

    feat (travelo) : implement responsive travel-search section

--- landing-pages/travelo/js/index.js
+++ landing-pages/travelo/js/index.js
@@ -1,9 +1,17 @@
 const menu = document.querySelector(".main-menu");
 const toggler = document.querySelector(".toggler");
+const selectTravelType = document.querySelector(".travel-type");
 
 const showMenu = () => {
   menu.classList.toggle("collapse");
 };
+
+const changeColor = () => {
+  selectTravelType.classList.add("text-white");
+};
 if (toggler && menu) {
   toggler.addEventListener("click", showMenu);
+}
+if (selectTravelType) {
+  selectTravelType.addEventListener("change", changeColor);
 }
